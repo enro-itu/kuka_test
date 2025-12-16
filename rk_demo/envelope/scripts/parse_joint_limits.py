7728a80e6dafa889eeaa3d62cd14e3395d3a30c0
yildirimar <yildirimar25@gmail.com>
2025-12-17 01:06:32 +0300

    Create parse_joint_limits.py

---
+++ rk_demo/envelope/scripts/parse_joint_limits.py
@@ -0,0 +1,7 @@
+joint_limits = {
+    'base_to_rotary': (-3.14, 3.14),
+    'rotary_to_lower': (-3.14, 3.14),
+    'lower_to_upper': (-3.14, 3.14),
+    'upper_to_support': (-3.14, 3.14),
+    'support_to_wrist': (-3.14, 3.14),
+}
