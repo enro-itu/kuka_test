e0070caa95056a863fcdd2e26f3cf5776bac6b24
itu-itis24-oflaz23 <oflaz23@itu.edu.tr>
2025-12-17 10:22:32 +0300

    Create arm_ui.launch.py

---
+++ arm_ui.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='rk_demo',
+            executable='arm_ui',
+            name='rk_arm_ui',
+            output='screen'
+        )
+    ])
